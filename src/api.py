6d9c97455c473b1a5a6106d93a3f44fde0f8e774
Andrey <yuryev@yandex.ru>
2023-10-27 19:24:27 +0300

    docstring and test template

--- src/api.py
+++ src/api.py
@@ -1,13 +1,18 @@
 from abc import ABC
 
 
-class Api(ABC):
+class API(ABC):
+    ''' класс для описания абстрактных методов API '''
     pass
 
 
-class SJApi(Api):
-    pass
+class SuperJobAPI(API):
+    ''' API для работы с вакансиями от superjob.ru '''
+    def __init__(self):
+        pass
 
 
-class HHApi(Api):
-    pass
+class HeadHunterAPI(API):
+    ''' API для работы с вакансиями от headhunter.ru '''
+    def __init__(self):
+        pass
---
+++ tests/test_api.py
@@ -0,0 +1,11 @@
+''' Тесты для классов API с помощью pytest'''
+import pytest
+from src.api import HeadHunterAPI, SuperJobAPI
+
+
+def test_superjob():
+    pass
+
+
+def test_headhunter():
+    pass

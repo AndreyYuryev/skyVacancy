bacc7a27e8f8d028d6d91a00225be9fe38621d3d
Andrey <yuryev@yandex.ru>
2023-10-27 21:18:05 +0300

    Read environment attributes with test case

--- src/api.py
+++ src/api.py
@@ -1,18 +1,46 @@
+import abc
 from abc import ABC
+from src.env import EnvParameter
 
 
 class API(ABC):
-    ''' класс для описания абстрактных методов API '''
-    pass
+    """ Класс для описания абстрактных методов API """
+
+    def __init__(self, test_mode=False):
+        """ Инициализировать атрибуты """
+        self.__test_mode = test_mode
+
+    @abc.abstractmethod
+    def get_api_key(self):
+        pass
+
+    @property
+    def test_mode(self):
+        """ Флаг тестового режима """
+        return self.__test_mode
 
 
 class SuperJobAPI(API):
-    ''' API для работы с вакансиями от superjob.ru '''
-    def __init__(self):
-        pass
+    """ API для работы с вакансиями от superjob.ru """
+
+    def __init__(self, test_mode=False):
+        """ Инициализировать атрибуты """
+        super().__init__(test_mode)
+
+    def get_api_key(self):
+        """ Получить api key для объекта с конструктором """
+        if super().test_mode:
+            return EnvParameter().api_key('TEST_SUPERJOB_API_KEY')
+        else:
+            return EnvParameter().api_key('SUPERJOB_API_KEY')
 
 
 class HeadHunterAPI(API):
-    ''' API для работы с вакансиями от headhunter.ru '''
-    def __init__(self):
-        pass
+    """ API для работы с вакансиями от headhunter.ru """
+
+    def get_api_key(self):
+        """ Получить api key для объекта без конструктора """
+        if self.test_mode:
+            return EnvParameter().api_key('TEST_HEADHUNTER_API_KEY')
+        else:
+            return EnvParameter().api_key('HEADHUNTER_API_KEY')
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,9 +3,13 @@ import pytest
 from src.api import HeadHunterAPI, SuperJobAPI
 
 
-def test_superjob():
-    pass
+def test_api_key():
+    """ Проверить чтение тестовых переменных окружения """
+    sj = SuperJobAPI(test_mode=True)
+    hh = HeadHunterAPI(test_mode=True)
+    assert sj.get_api_key() == 'TEST111'
+    assert hh.get_api_key() == 'TEST222'
 
 
-def test_headhunter():
+def test_value():
     pass

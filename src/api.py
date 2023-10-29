706c228ebddce59b5dd421989e62724edba08231
Andrey <yuryev@yandex.ru>
2023-10-30 00:45:14 +0300

    api class and test cases

--- src/api.py
+++ src/api.py
@@ -1,6 +1,9 @@
 import abc
-from abc import ABC
+from abc import ABC, abstractmethod
 from src.env import EnvParameter
+from src.req_params import RequestParameter
+import requests
+import json
 
 
 class API(ABC):
@@ -10,14 +13,36 @@ class API(ABC):
         """ Инициализировать атрибуты """
         self.__test_mode = test_mode
 
-    @abc.abstractmethod
-    def get_api_key(self):
-        pass
-
     @property
     def test_mode(self):
         """ Флаг тестового режима """
         return self.__test_mode
+
+    def _get_response_json(self, url=str(), params=dict(), headers=dict()):
+        """ Получить ответ на запрос и распарсить JSON """
+        rsp = requests.get(url=url, params=params, headers=headers)
+        rsp_json = rsp.json()
+        return rsp_json
+
+    @abstractmethod
+    def _get_api_key(self):
+        """ Получить ключ из переменных окружения """
+        pass
+
+    @abstractmethod
+    def _create_headers(self):
+        """ Создать словарь с заголовком запроса """
+        pass
+
+    @abstractmethod
+    def _create_params(self, request_params: RequestParameter):
+        """ Создать словарь с параметрами запроса """
+        pass
+
+    @abstractmethod
+    def get_vacancies(self, request_params: RequestParameter):
+        """ Получить словарь с вакансиями """
+        pass
 
 
 class SuperJobAPI(API):
@@ -26,21 +51,70 @@ class SuperJobAPI(API):
     def __init__(self, test_mode=False):
         """ Инициализировать атрибуты """
         super().__init__(test_mode)
+        self.__url = 'https://api.superjob.ru/2.0/vacancies/'
+        self.__headers = self._create_headers()
 
-    def get_api_key(self):
+    def _get_api_key(self):
         """ Получить api key для объекта с конструктором """
         if super().test_mode:
             return EnvParameter().api_key('TEST_SUPERJOB_API_KEY')
         else:
             return EnvParameter().api_key('SUPERJOB_API_KEY')
 
+    def _create_headers(self):
+        return {"X-Api-App-Id": self._get_api_key()}
+
+    def _create_params(self, request_params: RequestParameter):
+        params = dict()
+        params['count'] = request_params.count
+        params['page'] = request_params.page
+        params['archive'] = request_params.archive
+        # формирование запроса
+        # поиск везде по ключевым словам
+        params['keyword'] = ' '.join(request_params.search)
+
+        return params
+
+    def get_vacancies(self, request_params: RequestParameter):
+        vacancies = self._get_response_json(url=self.__url, headers=self.__headers,
+                                            params=self._create_params(request_params=request_params))
+        print(f'\n----- SuperJob-----')
+        for vacancy in vacancies['objects']:
+            print(vacancy['profession'])
+
 
 class HeadHunterAPI(API):
     """ API для работы с вакансиями от headhunter.ru """
 
-    def get_api_key(self):
+    def __init__(self, test_mode=False):
+        """ Инициализировать атрибуты """
+        super().__init__(test_mode)
+        self.__url = 'https://api.hh.ru/vacancies'
+        self.__headers = self._create_headers()
+
+    def _get_api_key(self):
         """ Получить api key для объекта без конструктора """
-        if self.test_mode:
+        if super().test_mode:
             return EnvParameter().api_key('TEST_HEADHUNTER_API_KEY')
         else:
             return EnvParameter().api_key('HEADHUNTER_API_KEY')
+
+    def _create_headers(self):
+        return {}
+
+    def _create_params(self, request_params: RequestParameter):
+        params = dict()
+        params['count'] = request_params.count
+        params['page'] = request_params.page
+        params['archive'] = request_params.archive
+        # формирование запроса
+        # поиск везде по ключевым словам
+        params['text'] = ' '.join(request_params.search)
+        return params
+
+    def get_vacancies(self, request_params: RequestParameter):
+        vacancies = self._get_response_json(url=self.__url, headers=self.__headers,
+                                            params=self._create_params(request_params=request_params))
+        print(f'\n-----HeadHunter-----')
+        for vacancy in vacancies['items']:
+            print(vacancy['name'])
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,15 +1,38 @@
 ''' Тесты для классов API с помощью pytest'''
 import pytest
 from src.api import HeadHunterAPI, SuperJobAPI
+from src.req_params import RequestParameter
 
 
 def test_api_key():
     """ Проверить чтение тестовых переменных окружения """
     sj = SuperJobAPI(test_mode=True)
     hh = HeadHunterAPI(test_mode=True)
-    assert sj.get_api_key() == 'TEST111'
-    assert hh.get_api_key() == 'TEST222'
+    assert sj._get_api_key() == 'TEST111'
+    assert hh._get_api_key() == 'TEST222'
 
 
-def test_value():
-    pass
+def test_params():
+    sj = SuperJobAPI()
+    hh = HeadHunterAPI()
+    req_parm = RequestParameter(search=['python', 'django'])
+    assert sj._create_params(req_parm) == {
+        "archive": False,
+        "count": 100,
+        "keyword": 'python django',
+        "page": 0,
+    }
+    assert hh._create_params(req_parm) == {
+        "archive": False,
+        "count": 100,
+        "page": 0,
+        "text": 'python django',
+    }
+
+
+def test_vacancies():
+    sj = SuperJobAPI()
+    hh = HeadHunterAPI()
+    req_parm = RequestParameter(search=['python', 'django'])
+    sj.get_vacancies(request_params=req_parm)
+    hh.get_vacancies(request_params=req_parm)

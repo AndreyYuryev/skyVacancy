a3d66a2db13b7d129b4dec53d6a7076e88b48a60
Andrey <yuryev@yandex.ru>
2023-11-01 16:19:06 +0300

    class parameters and test case

--- tests/test_req_params.py
+++ tests/test_req_params.py
@@ -1,11 +1,34 @@
 ''' Тесты для класса параметры запроса с помощью pytest'''
 import pytest
-from src.req_params import RequestParameter
+from src.req_params import RequestParameter, SearchParameter
+
+
+def test_search_parameter():
+    search = SearchParameter(keywords=['python', 'django'])
+    assert search.get_sj_params() == {
+        'keyword': 'python django'
+    }
+    assert search.get_hh_params() == {
+        'text': 'python django'
+    }
 
 
 def test_params():
-    request = RequestParameter(count=100, page=0, archive=False, search=['python', 'developer'])
+    search = SearchParameter(keywords=['python', 'django'])
+    request = RequestParameter(count=100, page=0, archive=False, search=search)
     assert request.count == 100
     assert request.page == 0
     assert not request.archive
-    assert request.search == ['python', 'developer']
+    assert request.search == search
+    assert request.params(api_sj=True) == {
+        'count': 100,
+        'page': 0,
+        'archive': False,
+        'keyword': 'python django'
+    }
+    assert request.params(api_hh=True) == {
+        'count': 100,
+        'page': 0,
+        'archive': False,
+        'text': 'python django'
+    }

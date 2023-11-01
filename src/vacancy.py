57f98b7174bc6391b3789351e4739b27c541562c
Andrey <yuryev@yandex.ru>
2023-11-01 13:15:11 +0300

    class vacancy and salary and test cases

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,39 +1,86 @@
-class Vacancy:
-    """ Описание найденной вакансии"""
+class Salary:
+    """ Зарплата вакансии"""
 
-    __Vacancies = []
-
-    def __init__(self, title='', salary={}, link='', city=''):
-        self.__title = title
-        if salary is not None:
-            self.__salary_from = int(salary.get('from', 0))
-            self.__salary_to = int(salary.get('to', 0))
+    def __init__(self, salary_from: int = 0, salary_to: int = 0, agreement: bool = False):
+        self.__from = salary_from
+        self.__to = salary_to
+        if self.__from == self.__to and self.__from == 0:
+            self.__agreement = True
         else:
-            self.__salary_from = 0
-            self.__salary_to = 0
-        self.__link = link
-        self.__city = city
-        Vacancy.__Vacancies.append(self)
+            self.__agreement = bool(agreement)
+
+    @property
+    def max_salary(self):
+        """ Если зарплата не по договоренности вернуть максимальную сумму
+        По договоренности вернуть 0
+        """
+        if self.is_agreement():
+            return 0
+        else:
+            salary_list = [self.__from, self.__to]
+            return max(salary_list)
+
+    def is_agreement(self):
+        """ Зарплата по договоренности """
+        return self.__agreement
 
     def __eq__(self, other):
-        """ Зарплаты равны """
-        if isinstance(other, Vacancy):
-            if max(self.salary) == max(other.salary):
+        """ Зарплаты равны
+        Если обе по договоренности
+        Если максимальные суммы равны
+        """
+        if isinstance(other, Salary):
+            if self.is_agreement():
+                return other.is_agreement()
+            if self.max_salary == other.max_salary:
                 return True
         return False
 
     def __lt__(self, other):
         """ Зарплата меньше """
-        if isinstance(other, Vacancy):
-            if max(self.salary) < max(other.salary):
+        if isinstance(other, Salary) and not self.is_agreement():
+            if self.max_salary < other.max_salary:
                 return True
         return False
 
     def __gt__(self, other):
         """ Зарплата больше """
-        if isinstance(other, Vacancy):
-            if max(self.salary) > max(other.salary):
+        if isinstance(other, Salary) and not self.is_agreement():
+            if self.max_salary > other.max_salary:
                 return True
+        return False
+
+
+class Vacancy:
+    """ Описание найденной вакансии"""
+
+    __Vacancies = []
+
+    def __init__(self, title='', salary=Salary(agreement=True), link='', city='', company='', description=''):
+        self.__title = title
+        self.__salary = salary
+        self.__link = link
+        self.__city = city
+        self.__company = company
+        self.__description = description
+        Vacancy.__Vacancies.append(self)
+
+    def __eq__(self, other):
+        """ Зарплаты равны """
+        if isinstance(other, Vacancy):
+            return self.__salary == other.__salary
+        return False
+
+    def __lt__(self, other):
+        """ Зарплата меньше """
+        if isinstance(other, Vacancy):
+            return self.__salary < other.__salary
+        return False
+
+    def __gt__(self, other):
+        """ Зарплата больше """
+        if isinstance(other, Vacancy):
+            return self.__salary > other.__salary
         return False
 
     @classmethod
@@ -50,7 +97,7 @@ class Vacancy:
     @property
     def salary(self):
         """ Зарплата """
-        return self.__salary_from, self.__salary_to
+        return self.__salary
 
     @property
     def link(self):
@@ -61,3 +108,13 @@ class Vacancy:
     def city(self):
         """ Город """
         return self.__city
+
+    @property
+    def company(self):
+        """ Наименование компании работодателя """
+        return self.__company
+
+    @property
+    def description(self):
+        """ Полное описание вакансии """
+        return self.__description
